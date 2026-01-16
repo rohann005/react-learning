f58428750392525b7f04992e8eb7248485f2a37c
Rohann005 <rohan971189@gmail.com>
2026-01-16 12:44:22 +0530

    make a card

--- react learning/src/App.jsx
+++ react learning/src/App.jsx
@@ -1,10 +1,9 @@
-
+import card from "../components/card" 
 const App = () => {
   return (
     <>
-    <h1>Hello World</h1>
-    <h2>Goodbye World</h2>
-    <h3>See you later!</h3>
+    < card />
+
     </>
   )
 }
